89ac0546f7a09f3ef63a2ededd0fdfb7093c48bf
siddak18 <siddakrajpal14@gmail.com>
2023-12-14 18:25:17 +0530

    infoupd

--- src/Components/Resume.tsx
+++ src/Components/Resume.tsx
@@ -2,6 +2,21 @@ import React from 'react'
 import Resumecard from './Resumecard'
 
 const Resume = () => {
+  const intern=[{
+    title:"Internship",
+    role:"Web Developer Intern",
+    organization:"Dhanaay",
+    duration:"Oct 2023 - Oct 2023",
+    description:"During my internship at Dhanaay, I gained valuable experience in frontend development. I learned extensively about Tailwind CSS and was tasked with enhancing their frontend UI. Additionally, I worked with data visualization libraries, including Tremor X Charts and Chart.js."
+  },
+  {
+    title:"Eduction",
+    role:"B.Tech, Mathamatics and Data Science",
+    organization:"National Institute of Technology, Bhopal",
+    duration:"Year 2022 - Year 2026",
+    description:"As an undergraduate student, I have a good understanding of web development technologies such as HTML, CSS, Tailwind CSS, JavaScript, and React JS. Additionally i also have skills in Java and problem-solving using Data structures and Algorithms."
+  }
+]
   return (
     <section  className='w-full flex flex-col justify-center items-center py-16'>
     <div id='resume' className='w-11/12'>
@@ -11,8 +26,14 @@ const Resume = () => {
       <p data-aos="fade-up" className='mt-4 font-medium text-3xl'> Here are my experiences and qualifications.</p>
       <div className='flex  w-full justify-center'>
       <div className=' flex mt-10 flex-col lg:flex-row  gap-10   w-full align-middle justify-between items-center'>
-      <Resumecard></Resumecard>
-      <Resumecard></Resumecard>
+        {
+          intern.map((item,idx)=>{
+            return(
+              <Resumecard {...item}></Resumecard>
+            )
+          })
+        }
+
       </div>
       </div>
       </div>
--- src/Components/Resumecard.tsx
+++ src/Components/Resumecard.tsx
@@ -1,9 +1,18 @@
+
 import React from 'react'
 
-const Resumecard = () => {
+interface inter{
+  title:String,
+  role:String,
+  organization:String,
+  duration:String,
+  description:String
+}
+
+const Resumecard:React.FC<inter> = (Info) => {
   return (
     <div data-aos="zoom-in-up" className='flex h-max w-full flex-col items-center justify-start py-2'>
-       <p className=' text-center w-40 p-2 h-max text-2xl font-semibold border-2 border-slate-500  text-nav rounded-xl'>Internship</p>
+       <p className=' text-center w-40 p-2 h-max text-2xl font-semibold border-2 border-slate-500  text-nav rounded-xl'>{Info.title} </p>
        <div className='flex h-max justify-between w-9/12 mt-10'>
         <div className='flex flex-col items-center align-bottom'>
          <div className="c1 w-3 h-3 rounded-full bg-white border-2 border-yellow-500"></div>
@@ -13,10 +22,10 @@ const Resumecard = () => {
          
         <div className='w-11/12 border-n rounded-lg border-nav flex justify-center py-5'>
             <div className=' w-11/12 py-3 '>
-            <p className='text-xl font-semibold'>Software Developer Intern</p>
-            <p className='text-md font-semibold text-slate-600'>Drafteq Engineering</p>
-            <p className='font-medium text-nav'>May 2023 - Aug 2023</p>
-            <p className=' font-medium text-slate-500 text-justify' >In my current internship at Drafteq Engineering, I'm working as a Full Stack Developer focusing on the frontend development using React JS, where I work on creating interactive and user-friendly interfaces. Additionally, I am responsible for building the backend API using Node.js, Express.js, and MongoDB, enabling data storage and retrieval for the application.</p>
+            <p className='text-xl font-semibold'> {Info.title} </p>
+            <p className='text-md font-semibold text-slate-600'> {Info.organization} </p>
+            <p className='font-medium text-nav'> {Info.duration} </p>
+            <p className=' font-medium text-slate-500 text-justify' > {Info.description} </p>
          </div>
          </div>
        </div>
